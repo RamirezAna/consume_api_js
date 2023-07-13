cf6a4e8531f4fc64f1d5cdcd57406583dfffd2e2
Ana Ramirez <aramirez.cac@gmail.com>
2023-07-13 15:32:12 -0400

    endpoints - query parameter

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
- 
-
- const URL ="https://api.thecatapi.com/v1/images/search";
+const API_URL ="https://api.thecatapi.com/v1/images/search?limit=2";
 
  /*
  fetch(URL)
@@ -10,16 +8,31 @@
     img.src = data[0].url;       
  });*/
 
-
- async function myCat () {
-     const res = await fetch(URL);
+/**/
+/* async function myCat () {
+     const res = await fetch(API_URL);
      const data = await res.json();
      const img = document.querySelector('img');
      img.src = data[0].url;
  }
  
  const myButton = document.querySelector("button");
- myButton.onclick = myCat;
+ myButton.onclick = myCat;*/
+ 
+async function reload(){
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    /*const img = document.querySelector('img');*/
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
 
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+
+}
+//para llamar directo cuando se ejecuta
+reload();
 
 
